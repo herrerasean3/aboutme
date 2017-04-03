0e3749df4c7a02dcfc8c33acdfa22a2fbc55ec48
herrerasean3 <herrerasean3@gmail.com>
2017-04-02 20:37:02 -0400

    Added further content to index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Stores the user's current day, month, and year when the site is loaded. convertDate merges the three different date variables into a single variable. displayDate then changes convertDate over to a string for neater insertion into a webpage.
-var currentDate = new Date(), m = (currentDate.getMonth() + 1), d = currentDate.getDate(), y = currentDate.getFullYear(), convertDate = m + ' - ' + d + ' - ' + y, displayDate = convertDate.toString();
+let currentDate = new Date(), m = (currentDate.getMonth() + 1), d = currentDate.getDate(), y = currentDate.getFullYear(), convertDate = m + ' - ' + d + ' - ' + y, displayDate = convertDate.toString();
 
 
 //Inserts the date string into the footer via the DOM. Uses textContent instead of innerHTML, as the MDN mentions the potential for cross site scripting attacks occuring with innerHTML.
